7c0dac651d8c69587013eeb5f4a8052e0e18e538
Trey <a1306575900@gmail.com>
2020-06-20 10:04:46 +0800

    improve jdbc cases

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,15 +1,9 @@
-import config.Config;
-import domain.Course;
-import domain.RoomOne;
-import domain.Student;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import domain.Info;
 import utils.JdbcUtil;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class Start {
     public static void main(String[] args) throws SQLException {
@@ -29,13 +23,15 @@ public class Start {
 //                roomOne2.getList()) {
 //            System.out.println(course);
 //        }
-        ResultSet rs = JdbcUtil.select();
+        ResultSet rs = JdbcUtil.select("SELECT * FROM TEST");
+        ArrayList<Info> infos = new ArrayList<Info>();
         System.out.println("id\tcourse_id\tstudent_id\tscore");
         while (rs.next()) {
-            String sno = rs.getString(1);
-            String course_id = rs.getString(2);
-            String student_id = rs.getString(3);
-            String score = rs.getString(4);
+            int sno = rs.getInt(1);
+            int course_id = rs.getInt(2);
+            int student_id = rs.getInt(3);
+            int score = rs.getInt(4);
+            infos.add(new Info(sno, course_id, student_id, score));
             System.out.println(sno + "\t" + course_id + "\t\t\t" + student_id + "\t\t" + score);
         }
         JdbcUtil.close(rs);
--- src/main/java/utils/JdbcUtil.java
+++ src/main/java/utils/JdbcUtil.java
@@ -8,19 +8,24 @@ import java.util.stream.Stream;
 
 public class JdbcUtil {
 
-    public static Properties jdbcConfig = new Properties();
-    public static Connection connection;
-    public static PreparedStatement statement;
+    private static final Properties jdbcConfig = new Properties();
+    private static Connection connection;
+    private static PreparedStatement statement;
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             InputStream inputStream = JdbcUtil.class.getClassLoader().getResourceAsStream("jdbcConfig.properties");
             if (inputStream != null){
                 jdbcConfig.load(inputStream);
-                String url = jdbcConfig.getProperty("url");
-                String usr = jdbcConfig.getProperty("username");
-                String password = jdbcConfig.getProperty("password");
-                connection = DriverManager.getConnection(url, usr, password);
+                String url = null;
+                String usr = null;
+                String password = null;
+                url = jdbcConfig.getProperty("url");
+                usr = jdbcConfig.getProperty("username");
+                password = jdbcConfig.getProperty("password");
+                if (!url.isEmpty() && !usr.isEmpty() && !password.isEmpty()) {
+                    connection = DriverManager.getConnection(url, usr, password);
+                }
             }
             System.out.println(connection);
         } catch (ClassNotFoundException | SQLException | IOException e) {
@@ -28,8 +33,7 @@ public class JdbcUtil {
         }
     }
 
-    public static ResultSet select() throws SQLException {
-        String sql = "select * from test";
+    public static ResultSet select(String sql) throws SQLException {
         statement = connection.prepareStatement(sql);
         return statement.executeQuery();
     }
